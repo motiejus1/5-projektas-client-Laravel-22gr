dcb55ea45fc94f013172bca27b8ffdce72a82526
motiejus1 <motiejus.baliukonis@vka.lt>
2022-01-21 21:02:47 +0200

    *summernote

--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -7,6 +7,12 @@
     <title>Edit Company</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+
+
 </head>
 <body>
     <div class="container">
@@ -25,8 +31,12 @@
                     @endif
                 @endforeach
             </select>    
-            <input  class="form-control" type='text' name="company_description" value="{{$company->description}}"  placeholder="Company Description"/>
-        
+            {{-- <input  class="form-control" type='text' name="company_description" value="{{$company->description}}"  placeholder="Company Description"/> --}}
+            
+            <textarea id="summernote" class="form-control" name="company_description">
+                {{$company->description}}
+            </textarea>
+
             @csrf
 
             <button class="btn btn-primary" type='submit'>Edit</button>
@@ -34,6 +44,10 @@
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-           
+    <script>
+        $(document).ready(function() {
+            $('#summernote').summernote();
+        });
+    </script>       
 </body>
 </html>
